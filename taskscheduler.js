c0d2e82e53750e62844aec37c1954ce1ea7c3c00
Stefaan Ponnet <stefaan@ponnet.com>
2017-10-10 16:09:41 +0200

    added timer to scheduler

--- taskscheduler.js
+++ taskscheduler.js
@@ -1,11 +1,35 @@
 const uuidv4 = require('uuid/v4');
 
 var tasks = {};
+var timerInterval;
 
 module.exports = {
 
 	tasks: tasks,
 
+	/**
+	 * start the timer scheduler
+	 * @return {Number} milliseconds to trigger 
+	 */
+	start: function(interval) {
+		timerInterval = setInterval(function() {
+			this.triggerInterval('timer')
+		}.bind(this), interval);
+	},
+
+	stop: function() {
+		if (timerInterval) {
+			clearInterval(timerInterval);
+		}
+	},
+
+	/**
+	 * add a task to the scheduler
+	 * @return {Promise} that resolves when task was added - resolves to task's id
+	 * @param {String} description of trigger type
+	 * @param {Object} data passed to 'task' function
+	 * @param {Function} function that gets called. should return a promise.
+	 */
 	addTask: function(interval, data, task) {
 		let id = uuidv4();
 		tasks[id] = {
@@ -18,35 +42,51 @@ module.exports = {
 		return Promise.resolve(id);
 	},
 
-	removeTask: function() {
+	/**
+	 * remove a task from the scheduler
+	 * @param {String} id of task to remove
+	 */
+	removeTask: function(id) {
 		if (tasks[id]) {
 			delete task[id];
 		}
 	},
 
+	/**
+	 * trigger a task from the scheduler
+	 * @param {String} type of tasks to trigger. the 'timer' type will be auto triggered.
+	 */
 	triggerInterval: function(type) {
 		var promises = [];
 		for (var p in tasks) {
 			if (tasks.hasOwnProperty(p) && tasks[p].interval === type) {
-				var promise = new Promise(function(resolve, reject) {
-					tasks[p].isRunning = true;
-					tasks[p].task(tasks[p].data).then(function() {
-						tasks[p].last_run = Date.now();
-						tasks[p].isRunning = false;
-						resolve();
-					}, function() {
-						reject();
+
+				if (type !== 'timer' || (type === 'timer' && (!tasks[p].last_run || Date.now() > tasks[p].last_run + tasks[p].data.interval))) {
+
+					var promise = new Promise(function(resolve, reject) {
+						tasks[p].isRunning = true;
+						tasks[p].task(tasks[p].data).then(function() {
+							tasks[p].last_run = Date.now();
+							tasks[p].isRunning = false;
+							resolve();
+						}, function() {
+							reject();
+						});
 					});
-				});
-				promises.push(promise);
+					promises.push(promise);
+
+				}
 			}
 		}
 		return Promise.all(promises);
 	},
-
-	reset: function(){
+	
+	/**
+	 * removes all tasks from the scheduler
+	 */
+	clear: function() {
 		for (var p in tasks) {
-			if (tasks.hasOwnProperty(p)){
+			if (tasks.hasOwnProperty(p)) {
 				delete tasks[p];
 			}
 		}
--- test/test-taskscheduler.js
+++ test/test-taskscheduler.js
@@ -5,11 +5,26 @@ describe("add task", function() {
 
   var client;
 
-  it('should add a task', function(done) {
+  // start scheduler with a 10ms interval check
+  scheduler.start(10);
+
+  it('should add a "block" task', function(done) {
     scheduler.addTask('block', {
       pubkey: '0x123'
     }, function(data) {
-      console.log('I am a task. my data is', data);
+      console.log('I am a block task. my data is', data);
+      return Promise.resolve();
+    }).then(function(taskid) {
+      console.log('task added. Id = ', taskid);
+      done();
+    })
+  });
+
+  it('should add a "timer" task', function(done) {
+    scheduler.addTask('timer', {
+      interval: 100 // ms
+    }, function(data) {
+      console.log('I am a timer task. my data is', data);
       return Promise.resolve();
     }).then(function(taskid) {
       console.log('task added. Id = ', taskid);
@@ -23,13 +38,19 @@ describe("add task", function() {
     });
   });
 
-  it('should show the task queue',function(done){
-    console.log(JSON.stringify(scheduler.tasks,null,4));
+  it('should wait 1000 ms', function(done) {
+    // allow the scheduler to trigger a few times.
+    setTimeout(done, 1000);
+  });
+
+  it('should show the task queue', function(done) {
+    console.log(JSON.stringify(scheduler.tasks, null, 4));
     done();
   });
 
   after(function(done) {
-    scheduler.reset();
+    scheduler.clear();
+    scheduler.stop();
     done();
   });
 
